e4fa9d61ab2c1c1ddcecef795ef823e5419fa722
Nanda Kumar <nk9219382@gmail.com>
2026-02-12 22:24:03 +0530

    feat: implement FastAPI application with a chat endpoint, rate limiting, and CORS, and rename `API_KEY` to `GOOGLE_API_KEY` in the example environment file.

--- app/main.py
+++ app/main.py
@@ -12,9 +12,16 @@ app = FastAPI()
 app.state.limiter = limiter
 app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "https://customer-support-agent-flax.vercel.app",
+    "https://customer-support-agent-flax.vercel.app/"
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
