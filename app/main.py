cca05dac310cc44cc2dea7d21fc959a84501c349
Nanda Kumar <nk9219382@gmail.com>
2026-02-12 20:11:38 +0530

    feat: Introduce FastAPI application with chat endpoint and update uvicorn entry point to `app.main:app`.

--- app/main.py
+++ app/main.py
@@ -1,31 +1,17 @@
-from langgraph import StateGraph, END
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_google_genai import GoogleGenAI
-from typing import TypedDict,Dict
-from langchain_core.runnables.graph import MermaidDrawMethod
-from langchain_core.output_parsers import StrOutputParser
-from dotenv import load_dotenv
-import os
-load_dotenv()
-class State(TypedDict):
+from fastapi import FastAPI
+from app.agent import app 
+from pydantic import BaseModel
+
+app=FastAPI()
+
+class Query(BaseModel):
     query:str
-    category:str
-    sentiment:str
-    response:str
-parser=StrOutputParser()
-llm=GoogleGenAI(model_name="gemini-3.0-flash-preview",api_key=os.getenv("GOOGLE_API_KEY"))
 
-def categorize(state:State) -> State:
-    '''
-    Categorize the query into a category
-    Technical, Billing, General
-    '''
-    prompt = ChatPromptTemplate.from_template("""
-    You are a customer support agent. Categorize the query into a category
-    Technical, Billing, General. Query: {query}
-    """)
-    chain=prompt | llm | parser
-    return {"category":chain.invoke({"query":state["query"]})}  
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
 
-
-
+@app.post("/chat")
+def chat(query:Query):
+    return app.invoke({"query":query.query})
+   
